9340f12bcbf4c385658da0f4e173074c9fe53f77
“Salil48” <salilchharia@gmail.com>
2023-02-14 19:54:52 +0530

    Sort Entries by City, State or Zip

--- AddressBookJavaScript.js
+++ AddressBookJavaScript.js
@@ -227,6 +227,35 @@ function sortContactsByName() {
         console.error(e);
     }
 }
+//Function to sort the contacts by City, State, or Zip (UC12)
+function sortContacts() {
+    try {
+        while (true) {
+            console.log("1: Sort By City Name \n2: Sort By State Name \n3: Sort By Zipcode \n4: Go Back");
+            switch (parseInt(readlineSync.question("Enter Your Choice From Above : "))) {
+                case 1:
+                    addressBookContactArr.sort((x, y) => x.city > y.city ? 1 : -1);
+                    console.log(`\nContacts Sorted By City Name : \n${addressBookContactArr.join("\n")}`)
+                    break;
+                case 2:
+                    addressBookContactArr.sort((x, y) => x.state > y.state ? 1 : -1);
+                    console.log(`\nContacts Sorted By State Name : \n${addressBookContactArr.join("\n")}`)
+                    break;
+                case 3:
+                    addressBookContactArr.sort((x, y) => x.zipCode > y.zipCode ? 1 : -1);
+                    console.log(`\nContacts Sorted By ZipCode : \n${addressBookContactArr.join("\n")}`)
+                    break;
+                case 4:
+                    return;
+                default:
+                    console.log("Wrong Choice")
+                    continue;
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 //Function to perform addressbook operations(UC3)
 function AddressBookOperations() {
@@ -235,7 +264,7 @@ function AddressBookOperations() {
         AddContact("Sumit", "Jagtap", "Chinchwad", "Pune", "Maharastra", "789456", "91 9087654321", "sumit@gmail.com");
         AddContact("Rita", "Bose", "Chinchner", "Satara", "Maharashtra", "415004", "91 8796543210", "rita5@gmail.com");
         while (true) {
-            console.log("\n0: Exit \n1: Add New Contact  \n2: Display contacts\n3.Find and edit contact\n4.Delete Contact using name\n5.Contact Count in the addressbook\n6.Search person by city or state\n7.view Person by city or state\n8.count by city or state\n9.Sort Entries by persons name");
+            console.log("\n0: Exit \n1: Add New Contact  \n2: Display contacts\n3.Find and edit contact\n4.Delete Contact using name\n5.Contact Count in the addressbook\n6.Search person by city or state\n7.view Person by city or state\n8.count by city or state\n9.Sort Entries by persons name\n10.Sort by city,state,zip");
             switch (parseInt(readlineSync.question('Enter the choice : '))) {
                 case 0:
                     console.log("Exited");
@@ -247,23 +276,25 @@ function AddressBookOperations() {
                 case 2:
                     DisplayContact();
                     break;
-                    case 3:
-                        FindAndEditContact();
-                        break;
-                        case 4:
-                            deleteContact();
-                            console.log("Contact deleted successfully");
-                            break;
-                            case 5:
-                                ContactCount();break;
-                                case 6:
-                                    searchPersonByCityOrState();break;
-                                case 7:
-                                    searchPersonByCityOrState();break;
-                                    case 8:
-                                        countPersonsByCityOrState();break;
-                                        case 9:
-                                            sortContactsByName();break;
+                case 3:
+                    FindAndEditContact();
+                    break;
+                case 4:
+                    deleteContact();
+                    console.log("Contact deleted successfully");
+                    break;
+                case 5:
+                    ContactCount();break;
+                case 6:
+                    searchPersonByCityOrState();break;
+                case 7:
+                    searchPersonByCityOrState();break;
+                case 8:
+                    countPersonsByCityOrState();break;
+                case 9:
+                    sortContactsByName();break;
+                case 10:
+                    sortContacts();break;
                 default:
                     console.log("Wrong Choice");
                     break;
